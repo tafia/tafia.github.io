c300c9ac1a9c26e556d863b1648dd98241ee295b
Johann Tuffe <tafia973@gmail.com>
2016-05-05 10:58:16 +0800

    update quick-xml docs

--- quick-xml/quick_xml/attributes/sidebar-items.js
+++ quick-xml/quick_xml/attributes/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Attributes","Iterator over attributes key/value pairs"],["UnescapedAttributes","Escaped attributesIterate over all attributes and unescapes attribute values"]]});
+initSidebarItems({"struct":[["Attributes","Iterator over attributes key/value pairs"],["UnescapedAttributes","Escaped attributes"]]});
