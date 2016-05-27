b08cb7515a7c0b868fe1f36214a7e28999e577e6
Johann Tuffe <tafia973@gmail.com>
2016-05-27 18:23:14 +0800

    update quick-csv docs

--- quick-csv/implementors/rustc_serialize/serialize/trait.Decoder.js
+++ quick-csv/implementors/rustc_serialize/serialize/trait.Decoder.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['quick_csv'] = ["impl&lt;'a&gt; <a class='trait' href='rustc_serialize/serialize/trait.Decoder.html' title='rustc_serialize::serialize::Decoder'>Decoder</a> for <a class='struct' href='quick_csv/columns/struct.Columns.html' title='quick_csv::columns::Columns'>Columns</a>&lt;'a&gt;",];
+implementors['quick_csv'] = ["impl&lt;'a&gt; <a class='trait' href='rustc_serialize/serialize/trait.Decoder.html' title='rustc_serialize::serialize::Decoder'>Decoder</a> for <a class='struct' href='quick_csv/columns/struct.Columns.html' title='quick_csv::columns::Columns'>Columns</a>&lt;'a&gt;",];implementors["quick_csv"] = ["impl&lt;'a&gt; <a class='trait' href='rustc_serialize/serialize/trait.Decoder.html' title='rustc_serialize::serialize::Decoder'>Decoder</a> for <a class='struct' href='quick_csv/columns/struct.Columns.html' title='quick_csv::columns::Columns'>Columns</a>&lt;'a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
