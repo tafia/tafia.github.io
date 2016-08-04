7677a897b3f4c4cd3480032a606e19e8c74fe76e
Johann Tuffe <tafia973@gmail.com>
2016-08-04 13:53:07 +0800

    update doc for quick-xml 0.2.4

--- quick-xml/implementors/quick_xml/trait.AsStr.js
+++ quick-xml/implementors/quick_xml/trait.AsStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['quick_xml'] = [];implementors['quick_xml'] = [];implementors["quick_xml"] = [];implementors["quick_xml"] = [];
+implementors['quick_xml'] = [];implementors['quick_xml'] = [];implementors["quick_xml"] = [];implementors["quick_xml"] = [];implementors["quick_xml"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
